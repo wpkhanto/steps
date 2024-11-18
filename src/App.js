24963c9d40a38dba90f435618ac0dfbe72775122
Watcharaphon Khanto <wpkhanto@icloud.com>
2024-11-18 19:08:05 +0700

    More Thoughts About State

--- src/App.js
+++ src/App.js
@@ -7,20 +7,30 @@ const messages = [
 ];
 
 export default function App() {
+    return (
+        <>
+            <Steps />
+            <Steps />
+        </>
+    );
+}
+function Steps() {
     const [step, setStep] = useState(1);
     const [isOpen, setIsOpen] = useState(true);
 
     function handlePrevious() {
-        if (step > 1) setStep(step - 1);
+        if (step > 1) setStep((s) => s - 1);
     }
 
     function handleNext() {
-        if (step < 3) setStep(step + 1);
+        if (step < 3) setStep((s) => s + 1);
     }
 
-    return (
-        <>
-        <button className="close" onClick={() => setIsOpen(!isOpen)}>&times;</button>
+    return (-
+        <div>
+            <button className="close" onClick={() => setIsOpen((is) => !is)}>
+                &times;
+            </button>
             {isOpen && (
                 <div className="steps">
                     <div className="numbers">
@@ -55,6 +65,6 @@ export default function App() {
                     </div>
                 </div>
             )}
-        </>
+        </div>
     );
 }
